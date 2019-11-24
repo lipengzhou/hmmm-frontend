f32657a73b2e50fae1cf8a61eec870ed59d8b9bf
lipengzhou <lpzmail@163.com>
2019-11-24 17:35:27 +0800

    init

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// import app from '@/module-dashboard/store/app'
+// import errorLog from '@/module-dashboard/store/errorLog'
+// import permission from '@/module-dashboard/store/permission'
+// import tagsView from '@/module-dashboard/store/tagsView'
+// import user from '@/module-dashboard/store/user'
+import getters from './getters'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+const store = new Vuex.Store({
+  getters
 })
+
+// const store = new Vuex.Store({
+//   modules: {
+//     app,
+//     errorLog,
+//     permission,
+//     tagsView,
+//     user
+//   },
+//   getters
+// })
+
+export default store
